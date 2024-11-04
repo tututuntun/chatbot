e43cd6315916a5be2f11ccb0f3e330e075b72ac7
Xuhui Ding <xuhui.ding@hotmail.com>
2024-11-03 19:18:22 -0800

    Added the logic to train the model with file and text

--- backend/services/geminiService.js
+++ backend/services/geminiService.js
@@ -1,25 +1,59 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleAIFileManager } = require("@google/generative-ai/server");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "../.env" });
 
 const apiKey = process.env.API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
-const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+const fileManager = new GoogleAIFileManager(apiKey);
 
 async function callGemini(promptContent, systemContent, previousChat) {
     try {
       const messages = [];
+
+      // https://stackoverflow.com/questions/78909501/gemini-api-error-400-invalid-or-unsupported-file-uri
+      const listFilesResponse = await fileManager.listFiles();
+      for (const file of listFilesResponse.files) {
+        console.log(`name: ${file.uri} | display name: ${file.displayName}`);
+      }
+
+      const uploadResult = await fileManager.uploadFile(
+        `files/Parafin Overview.pdf`,
+        {
+          mimeType: "application/pdf",
+          displayName: "Test content",
+        },
+      );
   
       const chat = model.startChat({
         history: [
           {
             role: "user",
-            parts: [{ text: "Hello" }],
+            parts: [
+              { 
+                // https://cloud.google.com/vertex-ai/generative-ai/docs/model-reference/inference#request
+                fileData: {
+                  fileUri: uploadResult.file.uri,
+                  mimeType: uploadResult.file.mimeType,
+                } 
+              }, 
+              { 
+                text: "You are NNRoad company customer service bot. \
+                  NNRoad is a leading international Employer of Record (EOR) service provider. \
+                  With our diverse remote workforce, we offer a technology-driven EOR platform that \
+                  enables companies to hire talented individuals from anywhere in the world, \
+                  without the hassle of setting up a local entity." 
+              },
+              { 
+                text: "Do not answer customer questions that are not related to NNRoad or Parafin." 
+              }
+            ],
           },
           {
             role: "model",
-            parts: [{ text: "Great to meet you. What would you like to know?" }],
+            parts: [{ text: "Great to meet you. How can I help you today?" }],
           },
         ],
       });
